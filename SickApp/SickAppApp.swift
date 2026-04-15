8b71c2236ebcf1f783990c951b8c899cab14e9a1
Parthee-Vijaya <pavi@kalundborg.dk>
2026-04-15 22:17:56 +0200

    Initial Commit

---
+++ SickApp/SickAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SickAppApp.swift
+//  SickApp
+//
+//  Created by Partheepan Vijayamohan on 15/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SickAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SickAppTests/SickAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  SickAppTests.swift
+//  SickAppTests
+//
+//  Created by Partheepan Vijayamohan on 15/04/2026.
+//
+
+import Testing
+@testable import SickApp
+
+struct SickAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
